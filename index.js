a1ffaa26291178bfea49ded2c9645bda1ab35820
Justin Morgan <justin.l.morgan@gmail.com>
2019-05-16 20:58:47 -0500

    Issue-1: Update dependencies
    closes #1
    SemVer:Minor
    enhancement

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 "use strict";
 
 // Required node modules
-var sh = require("child_process").exec;
+const sh = require("child_process").exec;
 
 // Required files
-var toJSON = require("./src/toJSON");
+const toJSON = require("./src/toJSON");
 
 /**
  * Loads the resolved git config from `git config -l`.
- * @requires child_process
- * @public
- * @function load
- * @param  {Function} callback - Method receiving the error or results from the processing of the config.
- * @return {Object}
+ *
+ * @param {Function} callback - Method receiving the error or results from the processing of the config.
+ * @require child_process
  */
 function load(callback) {
-  sh("git config -l", function (error, stdout, stderr) {
-    if (error) {
+  sh(
+    "git config -l",
+    (error, stdout, stderr) => (error ?
       callback({
-        error: error,
-        stderr: stderr
-      });
-    } else {
-      callback(null, toJSON(stdout));
-    }
-  });
+        error,
+        stderr
+      }) :
+      callback(null, toJSON(stdout)))
+  );
 }
 
 module.exports = load;
--- src/mergeSplitOption.js
+++ src/mergeSplitOption.js
@@ -2,18 +2,19 @@
 
 /**
  * Ensures stray newlines in the value for key=value pairs are not lost in the line splits.
+ *
  * @private
  * @function mergeSplitOption
- * @param  {Array} results - The destination array.
- * @param  {String} line - The potential key=value pair.
- * @param  {Number} index - Line index.
+ * @param {Array} results - The destination array.
+ * @param {String} line - The potential key=value pair.
+ * @param {Number} index - Line index.
  * @return {Array}
  */
 function mergeSplitOption(results, line, index) {
-  if (/^([^=]+)=/.test(line)) {
+  if (/^([^=]+)=/u.test(line)) {
     results.push(line);
   } else {
-    results[index - 1] += "\\n" + line;
+    results[index - 1] += `\\n${line}`;
   }
 
   return results;
